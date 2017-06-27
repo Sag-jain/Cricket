95a16a29c20f2aa6499161a3abdc405b30ff43c3
Sagar Jain <sagarjain5670@gmail.com>
2017-06-27 21:09:06 +0530

    new release

---
+++ script/myscript.js
@@ -0,0 +1,7 @@
+var showText = function (target, message, index, interval)
+ {   
+  if (index < message.length) {
+    $(target).append(message[index++]);
+    setTimeout(function () { showText(target, message, index, interval); }, interval);
+  }
+ }
